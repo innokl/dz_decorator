ee1c86e7a7c10b50b7f75e03e9ff6fe15305590f
innokl <84169849+innokl@users.noreply.github.com>
2026-04-20 21:33:11 +0300

    Update Decorator.java

--- src/decorator/Decorator.java
+++ src/decorator/Decorator.java
@@ -3,7 +3,7 @@ package decorator;
 import dish.Dish;
 
 public abstract class Decorator implements Dish {
-    protected Dish dish;
+    private Dish dish;
 
     public Decorator(Dish dish) {
         this.dish = dish;

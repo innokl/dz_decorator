ade5ae236ba0976e61e7dd1917c92e3c5d316f1f
innokl <84169849+innokl@users.noreply.github.com>
2026-04-20 22:00:55 +0300

    Update Berries.java

--- src/decorator/Berries.java
+++ src/decorator/Berries.java
@@ -11,11 +11,11 @@ public class Berries extends Decorator {
 
     @Override
     public String getDescription() {
-        return dish.getDescription() + " + Снежные ягоды";
+        return super.getDish().getDescription() + " + Снежные ягоды";
     }
 
     @Override
     public int getCost() {
-        return dish.getCost() + 6;
+        return super.getDish().getCost() + 6;
     }
 }
